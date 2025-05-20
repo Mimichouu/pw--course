dc0d76445802e426211086d79e15de13ec0a95e3
Hang <nthang0819@gmail.com>
2025-05-20 11:16:23 +0700

    feat:Lesson2

--- tests/lesson-2/ex5.js
+++ tests/lesson-2/ex5.js
@@ -1,11 +1,8 @@
-let i = 1
-
-for (i = 1; i <= 100; i++) {
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
     if (i % 2 === 0) {
-        console.log('Số " + i + "là số chẵn');
+        console.log("Số " + i + "là số chẵn");
     } else {
-        console.log('Số " + i + "là số lẻ');
+        console.log("Số " + i + "là số chẵn");
     }
 }
 
