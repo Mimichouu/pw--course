4c56dbbe2b8b968de8e68aa08eb2e7cd6bc9d34a
Hang <nthang0819@gmail.com>
2025-05-20 10:13:02 +0700

    feat: Lesson2

---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,8 @@
+const chieuCao = 155;
+const soLeChieuCao = chieuCao % 100;
+const canNangLyTuong = (soLeChieuCao * 9) / 10;
+const canNangToiThieu = (soLeChieuCao *8) / 10;
+const CanNangToida = soLeChieuCao;
+
+console.log(`canNangLyTuong ${canNangLyTuong}, CanNangToida ${CanNangToida}, canNangToiThieu ${canNangToiThieu}`);
+
