440655c8af26a551e627016f3560babf9b800631
Hang <nthang0819@gmail.com>
2025-05-20 09:31:55 +0700

    feat: Lesson2

---
+++ tests/lesson-2/ex3.js
@@ -0,0 +1,9 @@
+const i = 10;
+
+if (i > 0) {
+    console.log('Giá trị bạn nhập là số dương');
+} else if (i < 0) {
+    console.log('Giá trị bạn nhập là số âm');
+} else {
+    console.log('Giá trị bạn nhập bằng 0');
+}
