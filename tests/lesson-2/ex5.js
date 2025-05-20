2a0b788ddb7c8282ebc150fceb9d9220bb5d7c20
Hang <nthang0819@gmail.com>
2025-05-20 10:59:23 +0700

    feat: Lesson2

--- tests/lesson-2/ex5.js
+++ tests/lesson-2/ex5.js
@@ -3,9 +3,9 @@ let i = 1
 for (i = 1; i <= 100; i++) {
     console.log(i);
     if (i % 2 === 0) {
-        console.log('Số <1> là số chẵn');
+        console.log('Số " + i + "là số chẵn');
     } else {
-        console.log('Số <i> là số lẻ');
+        console.log('Số " + i + "là số lẻ');
     }
 }
 
