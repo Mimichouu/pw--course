f6100c5d6eb59f604e156b814adc9d30345d4869
Hang <nthang0819@gmail.com>
2025-06-24 10:33:04 +0700

    feat: lesson-8

--- tests/lesson-8/Media.spec.ts
+++ tests/lesson-8/Media.spec.ts
@@ -7,19 +7,16 @@ test.describe('MEDIA - @MEDIA_FILE_001', async () => {
         password: 'zZ2H#GD$&Hp3oNrT5V*1oPl2'
     };
 
-
     let xpath = {
         userName: '//input[@id="user_login"]',
         password: '//input[@id="user_pass"]',
         BtnLogIn: '//input[@id="wp-submit"]',
         MenuMedia: '//li[@id="menu-media"]',
         BtnAddMediaFile: '//a[@role="button"]',
-        BtnSelectFile: '//button[@type="button"]',
         InputFile: '//input[@type="file"]',
-        fileName: '//li[@aria-label="hang"]', 
-        BtnDeleteFile: '//button[@type="button"]'
+        fileName: '//h2[text()="Media list"]/following::li[@aria-label="embe"]', 
+        BtnDeleteFile: '//a[@class="view-attachment"]/following-sibling::button[text()="Delete permanently"]'
     };
-
     
     test.beforeEach(async ({page}) => {
         await test.step('Login và vào menu category', async () => {
@@ -35,28 +32,30 @@ test.describe('MEDIA - @MEDIA_FILE_001', async () => {
         });
     });
 
-
     test('Media-upload file success', async ({page}) => {
 
-        const filePath = 'tests/lesson-8/hang.txt';
+        const filePath = 'data-test/embe.jpg';
 
         // Thực hiện upload ảnh
         await test.step('upload ảnh', async () => {
             await page.click(xpath.BtnAddMediaFile);
             await page.locator(xpath.InputFile).setInputFiles(filePath);
-            });
-    
-        // F5 trang
-        await test.step('f5 trang', async () => {
-            await page.reload();
-            await expect(page.locator(xpath.fileName)).toBeVisible({timeout: 10000});
         });
-
+        
+        // // F5 trang
+        // await test.step('f5 trang', async () => {
+        //     await page.reload();
+        //     await page.waitForLoadState("load", {timeout: 20000});
+        //     await expect(page.locator(xpath.fileName)).toBeVisible();
+        // });
+        
         // xóa dữ liệu
         await test.step('Xóa dữ liệu', async () => {
             await page.locator(xpath.fileName).click();
             await page.locator(xpath.BtnDeleteFile).click();
             await expect(page.locator(xpath.fileName)).not.toBeVisible();
         });
-    });
-});
+    }); 
+}); 
+
+
